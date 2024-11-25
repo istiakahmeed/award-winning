d2211b5f1623a61eaec942d64de55f3c989bb8ea
Istiak Ahmed <istiakahmed6219@gmail.com>
2024-11-25 11:37:14 +0600

    complete all work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
+import Story from "./components/Story";
 
 const App = () => {
   return (
@@ -8,6 +12,10 @@ const App = () => {
       <NavBar />
       <Hero />
       <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
